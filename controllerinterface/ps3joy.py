741eb71d5545afd7d4f01c73bf34c7ef977bd048
Sindre Hansen <sindrehansen92@gmail.com>
2015-11-05 23:53:47 +0100

    Some testing of the ps3-controller interface

--- controllerinterface/ps3joy.py
+++ controllerinterface/ps3joy.py
@@ -1,1 +1,28 @@
-#test
+import time
+import os
+import sys
+import pygame
+
+# Re-direct our output to standard error, we need to ignore standard out to hide some nasty print statements from pygame
+sys.stdout = sys.stderr
+
+# Setup pygame
+os.environ["SDL_VIDEODRIVER"] = "dummy" # Removes the need to have a GUI window
+pygame.init()
+pygame.joystick.init()
+joystick = pygame.joystick.Joystick(0)
+joystick.init()
+
+try:
+    print "Press CTRL+C to quit"
+    # events = pygame.event.get()
+    while True:
+        print 
+        for i in range (20):
+           print "Button {0}: {1}\n".format(i, pygame.get_button(i))
+        
+
+    
+except KeyboardInterrupt:
+    #CTRL+C exit
+    print
