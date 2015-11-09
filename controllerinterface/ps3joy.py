1301a42b25f091588382905eda4cf0f27c1aa71a
Sindre Hansen <sindrehansen92@gmail.com>
2015-11-10 00:36:58 +0100

    Fixed joystick interace, it works!

--- controllerinterface/ps3joy.py
+++ controllerinterface/ps3joy.py
@@ -15,23 +15,55 @@ joystick.init()
 
 clear = lambda: os.system('clear')
 
+def print_axis(axis):
+    if values[axis] >= 0:
+        pos = int (values[axis]*64)
+        print "[", " "*64, " "*pos, "#", " "*(64-pos), "]" 
+        #print "Axis", axis, ": ", pos
+    else:
+        pos = int( values[axis]*-64)
+        #print "Axis", axis, ": ", pos
+        print "[", " "*(64-pos),"#", " "*pos, " "*64, "]" 
 try:
     values = [0]*23
+    name = joystick.get_name()
     axes = joystick.get_numaxes()
     buttons = joystick.get_numbuttons()
-    while True:
-        #hadEvent = False
-        #events = pygame.event.get()
-        #for event in events:
-        #    if event.type = JOYBUTTONDOWN:
+    running = True
+    while running:
+        hadEvent = False
+        buttonPressed = False
+        buttonReleased = False
+        events = pygame.event.get()
+        # for event in events:
+        #     if event.type == pygame.QUIT:
+        #          print "HEI"
+                 
+        #     if event.type == pygame.JOYBUTTONDOWN:
+        #         hadEvent = True
+        #         buttonPressed = True
+        #     if event.type == pygame.JOYBUTTONUP:
+        #         hadEvent = True
+        #         buttonReleased = True
+
         for i in range(4):
+            # temp = joystick.get_axis(i)
+            # temp *= 255
+            # values[i] = int(temp)
             values[i] = joystick.get_axis(i)
         for i in range(16):
             values[i+4] = joystick.get_button(i)
 
+        if values[7]:
+            running = False
+
         clear()
-        print "Press CTRL+C to quit"
-        print "Axes: ", axes
+        print "Press CTRL+C or 'start' to quit"
+        print "Name: ", name
+        print "Axes: "
+        for i in range(4):
+            print_axis(i)
+        
         print "Buttons: ", buttons
         print values
         time.sleep(0.1)
