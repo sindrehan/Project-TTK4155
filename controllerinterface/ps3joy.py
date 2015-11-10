7ef883dd9aa66a6eec6b140bd90f5e91be6658f9
Sindre Hansen <sindrehansen92@gmail.com>
2015-11-10 23:18:18 +0100

    Minor changes to ps3joy.py
    Mostly formatting of print output etc.

--- controllerinterface/ps3joy.py
+++ controllerinterface/ps3joy.py
@@ -3,6 +3,8 @@ import os
 import sys
 import pygame
 
+print_width = 32
+
 # Re-direct our output to standard error, we need to ignore standard out to hide some nasty print statements from pygame
 sys.stdout = sys.stderr
 
@@ -17,13 +19,11 @@ clear = lambda: os.system('clear')
 
 def print_axis(axis):
     if values[axis] >= 0:
-        pos = int (values[axis]*64)
-        print "[", " "*64, " "*pos, "#", " "*(64-pos), "]" 
-        #print "Axis", axis, ": ", pos
+        pos = int (values[axis]*print_width)
+        print "["+" "*print_width+" "*pos+"#"+" "*(print_width-pos-1)+"]" 
     else:
-        pos = int( values[axis]*-64)
-        #print "Axis", axis, ": ", pos
-        print "[", " "*(64-pos),"#", " "*pos, " "*64, "]" 
+        pos = int( values[axis]*-print_width)
+        print "["+" "*(print_width-pos)+"#"+" "*pos+" "*(print_width-1)+"]" 
 try:
     values = [0]*23
     name = joystick.get_name()
@@ -34,22 +34,11 @@ try:
         hadEvent = False
         buttonPressed = False
         buttonReleased = False
+        
+        #Clear the event stack to prevent overflow
         events = pygame.event.get()
-        # for event in events:
-        #     if event.type == pygame.QUIT:
-        #          print "HEI"
-                 
-        #     if event.type == pygame.JOYBUTTONDOWN:
-        #         hadEvent = True
-        #         buttonPressed = True
-        #     if event.type == pygame.JOYBUTTONUP:
-        #         hadEvent = True
-        #         buttonReleased = True
 
         for i in range(4):
-            # temp = joystick.get_axis(i)
-            # temp *= 255
-            # values[i] = int(temp)
             values[i] = joystick.get_axis(i)
         for i in range(16):
             values[i+4] = joystick.get_button(i)
@@ -60,13 +49,18 @@ try:
         clear()
         print "Press CTRL+C or 'start' to quit"
         print "Name: ", name
-        print "Axes: "
+        print "Number of axes:", axes 
+        print "Number of buttons: ", buttons
+
         for i in range(4):
             print_axis(i)
-        
-        print "Buttons: ", buttons
-        print values
-        time.sleep(0.1)
+
+        print "Buttons: "
+        for i in values[4:]:
+            print i, 
+
+        #Sleep to prevent flickering
+        time.sleep(0.05)
 
 except KeyboardInterrupt:
     #CTRL+C exit
